a37534d2d5562ad583214f5dba3e55880c59dc3c
Cédric <nifix@users.noreply.github.com>
2019-05-07 23:32:08 +0200

    Changed production mode on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 
-    mode: "development",
+    mode: "pruduction",
     entry: "./app/app.js",
     output: {
         path: path.resolve(__dirname, "dist"),
