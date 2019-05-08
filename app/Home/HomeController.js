276e5836ad1e8b4f7295e0f0ce65fd1b3cd0e3e8
Cédric <nifix@users.noreply.github.com>
2019-05-08 02:18:12 +0200

    Changed comment to be more explicit

--- app/Home/HomeController.js
+++ app/Home/HomeController.js
@@ -9,7 +9,12 @@
 
 export function HomeController($scope) {
 
-  // Mini func to test AngularJS 1.7.8
+  /** Mini func to test AngularJS 1.7.8
+    * Tout le monde sait très bien que je n'ai pas besoin de watch
+    * cette variable pour faire ce que je fais, qu'il suffirait que je
+    * ng-bind, mais pour test correctement qu'Angular marchait
+    * correctement, j'ai fait un truc inutile.
+  */
   
   $scope.$watch('testy', function() {
     $scope.test = $scope.testy
